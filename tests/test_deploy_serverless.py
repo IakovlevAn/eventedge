c485a0342d40b5f3c431b2c8992f96e95c07087b
Andrey Yakovlev <119817919+IakovlevAn@users.noreply.github.com>
2026-08-10 18:40:41 +0300

    Make filters responsive and backfill reliable (#81)

--- tests/test_deploy_serverless.py
+++ tests/test_deploy_serverless.py
@@ -43,7 +43,7 @@ def test_deployment_payload_has_budget_caps() -> None:
         "YANDEX_GPT_FOLDER_ID": "folder-id",
         "YANDEX_GPT_MODEL": "yandexgpt-lite",
         "BACKFILL_BATCH_LIMIT": "4",
-        "BACKFILL_CONCURRENCY": "4",
+        "BACKFILL_CONCURRENCY": "1",
         "EVENTEDGE_MONTHLY_BUDGET_RUB": "15000",
     }
 
